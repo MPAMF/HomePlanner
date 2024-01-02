e2f481ffec7e9f169488f8c30f39516cd6148dae
qgerling <quentin.gerling@etu.unistra.fr>
2024-01-02 23:24:10 +0100

    Creation of the zoom and dezoom command

---
+++ src/app/core/commands/canvas-commands.ts
@@ -0,0 +1,57 @@
+import {Command} from "./command";
+import {Point} from "../models/point";
+
+export class MoveElementCommand extends Command {
+
+  constructor(private delta: Point) {
+    super();
+  }
+
+  override execute(): void {
+    this.board.offset.x += this.delta.x;
+    this.board.offset.y += this.delta.y;
+  }
+
+  override undo(): void {
+    this.board.offset.x -= this.delta.x;
+    this.board.offset.y -= this.delta.y;
+  }
+}
+
+export class ZoomCommand extends Command {
+
+  constructor(private scaleFactor: number) {
+    super();
+  }
+
+  override execute(): void {
+    if(this.canvasCtx){
+      this.canvasCtx.scale(this.scaleFactor, this.scaleFactor);
+    }
+  }
+
+  override undo(): void {
+    if(this.canvasCtx){
+      this.canvasCtx.scale(1 / this.scaleFactor, 1 / this.scaleFactor);
+    }
+  }
+}
+
+export class DeZoomCommand extends Command {
+
+  constructor(private scaleFactor: number) {
+    super();
+  }
+
+  override execute(): void {
+    if(this.canvasCtx){
+      this.canvasCtx.scale(1 / this.scaleFactor, 1 / this.scaleFactor);
+    }
+  }
+
+  override undo(): void {
+    if(this.canvasCtx){
+      this.canvasCtx.scale(this.scaleFactor, this.scaleFactor);
+    }
+  }
+}
--- src/app/core/models/board.ts
+++ src/app/core/models/board.ts
@@ -26,6 +26,18 @@ export class Board implements Drawable {
       ctx.save();
     }
 
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    if (preserveTransform) {
+      ctx.restore();
+    }
+  }
+
+  private reset(ctx: CanvasRenderingContext2D, preserveTransform: boolean = false) {
+    if (preserveTransform) {
+      ctx.save();
+    }
+
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
